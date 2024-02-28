d349451f93eba934c089869e3487487de9f3261b
Ardhirika <161566714+Ardhirika@users.noreply.github.com>
2024-02-28 14:58:11 +0700

    Add files via upload

---
+++ name = Ardhi.py
@@ -0,0 +1,13 @@
+name = "Ardhi"
+age = "18"
+major = "informatic engineering"
+live = "Kadudampit"
+hobby = "badminton" 
+gender = "male"
+
+print("my name is =",name)
+print("my age =",age)
+print("my major =", major)
+print("i live in =", live)
+print("my hobby =", hobby)
+print("gender =", gender)
